25def5346e9a861c2e8bda57994af9981111dcbb
DaikoGames <136380205+DaikoGames@users.noreply.github.com>
2026-04-12 16:46:54 +0200

    Updated installer logic

--- Installprerequesitaries.bat
+++ Installprerequesitaries.bat
@@ -1,3 +1,4 @@
 cd 
-./Install/dotnet-install.bat --runtime dotnet --version 6.0.0
-./Install/InstallAvalonia.bat
+./Install/dotnet-install.ps1 --runtime dotnet --version 6.0.0
+./Install/InstallAvalonia.ps1
+./Install/Install-Node-and-NPM.ps1
--- Installprerequesitaries.sh
+++ Installprerequesitaries.sh
@@ -1,3 +1,4 @@
 cd 
 ./Install/dotnet-install.sh --runtime dotnet --version 6.0.0
 ./Install/InstallAvalonia.sh
+./Install/Install-Node-and-NPM.sh
