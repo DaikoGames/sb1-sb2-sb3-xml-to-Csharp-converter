2a9be27744ae6bd20ed45abb1e250f0966ee27cf
DaikoGames <136380205+DaikoGames@users.noreply.github.com>
2026-05-01 16:15:38 +0200

    Delete Installprerequesitaries.ps1

--- Installprerequesitaries.ps1
+++
@@ -1,7 +0,0 @@
-cd
-cd ./Install
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-./Install-Chocolately.ps1
-./dotnet-install.ps1 -Runtime dotnet -Version 9.0.0
-./InstallAvalonia.ps1
-./Install-Node-and-NPM.ps1
