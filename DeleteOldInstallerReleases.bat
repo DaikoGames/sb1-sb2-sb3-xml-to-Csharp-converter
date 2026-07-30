bc1f23c621ea5ca6229f76285f0d60ecbc5b109d
DaikoGames <136380205+DaikoGames@users.noreply.github.com>
2026-07-30 15:14:06 +0200

    Delete DeleteOldInstallerReleases.bat

--- DeleteOldInstallerReleases.bat
+++
@@ -1,6 +0,0 @@
-rmdir /s /q .\bin\Release\net9.0\win-x64\Releases
-rmdir /s /q .\bin\Release\net9.0\win-x86\Releases
-rmdir /s /q .\bin\Release\net9.0\linux-x64\Releases
-rmdir /s /q .\bin\Release\net9.0\linux-arm64\Releases
-rmdir /s /q .\bin\Release\net9.0\osx-x64\Releases
-rmdir /s /q .\bin\Release\net9.0\osx-arm64\Releases
